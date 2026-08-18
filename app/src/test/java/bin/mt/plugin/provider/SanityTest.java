44f88cbb79e420758fa93ea578e2ae7997ebf05b
Ilker Binzet <ilker.binzet@gmail.com>
2026-08-17 22:36:52 -0500

    test: add unit test source set with JUnit 4

---
+++ app/src/test/java/bin/mt/plugin/provider/SanityTest.java
@@ -0,0 +1,13 @@
+package bin.mt.plugin.provider;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SanityTest {
+
+    @Test
+    public void unitTestSourceSetRuns() {
+        assertTrue("the unit test source set is wired up", true);
+    }
+}
